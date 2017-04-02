f149f59b9f5fc9bac36d97bf11386344bf397ac1
Hekili <Hekili@users.noreply.github.com>
2017-04-02 13:17:15 -0400

    Try updated LibArtifactData-1.0...

--- Hekili.lua
+++ Hekili.lua
@@ -13,7 +13,7 @@ ns.PTR = GetBuildInfo() ~= "7.2.0"
 ns.lib = {
     AceConfig = LibStub( "AceConfig-3.0" ),
     AceConfigDialog = LibStub( "AceConfigDialog-3.0" ),
-    ArtifactData = LibStub( "LibArtifactData-1.0h" ),
+    ArtifactData = LibStub( "LibArtifactData-1.0" ),
     -- LegionArtifacts = LibStub:GetLibrary( "LegionArtifacts-1.1" ),
 	Format = {}, -- filled by Formatting.lua
 	LibDualSpec = LibStub( "LibDualSpec-1.0" ),
