e2f98e348eb2eda8cfef5e7ed96a7e9b5b852916
Hekili <Hekili@users.noreply.github.com>
2018-06-21 10:53:45 -0400

    Update .pkgmeta to get newer Ace3 versions (should fix AceComm/ChatThrottleLib error). Update date-stamp on Action Packs when (re-)importing. Remove unneeded "error" messages on empty script conditions. Add pet.whatever.exists syntax. Drop LibDualSpec. Retribution APL update. Add preliminary Demonology Warlock module (lacks basically all mechanics at this time).

---
+++ Classes/WarlockDemonology.lua
@@ -0,0 +1,800 @@
+-- WarlockDemonology.lua
+-- June 2018
+
+local addon, ns = ...
+local Hekili = _G[ addon ]
+
+local class = Hekili.Class
+local state = Hekili.State
+
+
+if UnitClassBase( 'player' ) == 'WARLOCK' then
+    local spec = Hekili:NewSpecialization( 266 )
+
+    spec:RegisterResource( Enum.PowerType.SoulShards )
+    spec:RegisterResource( Enum.PowerType.Mana )
+    
+    -- Talents
+    spec:RegisterTalents( {
+        dreadlash = 19290, -- 264078
+        demonic_strength = 22048, -- 267171
+        bilescourge_bombers = 23138, -- 267211
+
+        demonic_calling = 22045, -- 205145
+        power_siphon = 21694, -- 264130
+        doom = 23158, -- 265412
+
+        demon_skin = 19280, -- 219272
+        burning_rush = 19285, -- 111400
+        dark_pact = 19286, -- 108416
+
+        from_the_shadows = 22477, -- 267170
+        soul_strike = 22042, -- 264057
+        summon_vilefiend = 23160, -- 264119
+
+        darkfury = 22047, -- 264874
+        mortal_coil = 19291, -- 6789
+        demonic_circle = 19288, -- 268358
+
+        soul_conduit = 23147, -- 215941
+        inner_demons = 23146, -- 267216
+        grimoire_felguard = 21717, -- 111898
+
+        sacrificed_souls = 23161, -- 267214
+        demonic_consumption = 22479, -- 267215
+        nether_portal = 23091, -- 267217
+    } )
+
+
+    -- Auras
+    spec:RegisterAuras( {
+        axe_toss = {
+            id = 89766,
+            duration = 4,
+            max_stack = 1,
+        },
+
+        bile_spit = {
+            id = 267997,
+            duration = 10,
+            max_stack = 1,
+        },
+
+        burning_rush = {
+            id = 111400,
+            duration = 3600,
+            max_stack = 1,
+        },
+
+        dark_pact = {
+            id = 108416,
+            duration = 20,
+            max_stack = 1,
+        },
+
+        demonic_calling = {
+            id = 205146,
+            duration = 20,
+            type = "Magic",
+            max_stack = 1,
+        },
+
+        demonic_circle = {
+            id = 48018,
+        },
+
+        demonic_circle_teleport = {
+            id = 48020,
+        },
+
+        demonic_core = {
+            id = 264173,
+            duration = 20,
+            max_stack = 4,
+        },
+
+        demonic_power = {
+            id = 265273,
+            duration = 15,
+            max_stack = 1,
+        },
+
+        drain_life = {
+            id = 234153,
+            duration = 5,
+            max_stack = 1,
+        },
+
+        eye_of_guldan = {
+            id = 272131,
+            duration = 15,
+            max_stack = 1,
+        },
+
+        eye_of_kilrogg = {
+            id = 126,
+        },
+
+        fear = {
+            id = 118699,
+            duration = 20,
+            type = "Magic",
+            max_stack = 1,
+        },
+
+        from_the_shadows = {
+            id = 270569,
+            duration = 12,
+            max_stack = 1,
+        },
+
+        legion_strike = {
+            id = 30213,
+            duration = 6,
+            max_stack = 1,
+        },
+
+        mortal_coil = {
+            id = 6789,
+            duration = 3,
+            type = "Magic",
+            max_stack = 1,
+        },
+
+        ritual_of_summoning = {
+            id = 698,
+        },
+
+        shadowfury = {
+            id = 30283,
+            duration = 3,
+            type = "Magic",
+            max_stack = 1,
+        },
+
+        soul_leech = {
+            id = 108366,
+            duration = 15,
+            max_stack = 1,
+        },
+
+        soul_link = {
+            id = 108415,
+        },
+
+        soul_shards = {
+            id = 246985,
+        },
+
+        unending_breath = {
+            id = 5697,
+            duration = 600,
+            max_stack = 1,
+        },
+
+        unending_resolve = {
+            id = 104773,
+            duration = 8,
+            max_stack = 1,
+        },
+
+    } )
+
+    -- Abilities
+    spec:RegisterAbilities( {
+        axe_toss = {
+            id = 119914,
+            cast = 0,
+            cooldown = 30,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 236316,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        banish = {
+            id = 710,
+            cast = 1.5,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136135,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        bilescourge_bombers = {
+            id = 267211,
+            cast = 0,
+            cooldown = 30,
+            gcd = "spell",
+            
+            spend = 2,
+            spendType = "soul_shards",
+            
+            startsCombat = true,
+            texture = 132182,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        burning_rush = {
+            id = 111400,
+            cast = 0,
+            cooldown = 0,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 538043,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        call_dreadstalkers = {
+            id = 104316,
+            cast = 2,
+            cooldown = 20,
+            gcd = "spell",
+            
+            spend = 2,
+            spendType = "soul_shards",
+            
+            startsCombat = true,
+            texture = 1378282,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        command_demon = {
+            id = 119898,
+            cast = 0,
+            cooldown = 0,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 236292,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        create_healthstone = {
+            id = 6201,
+            cast = 3,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 538745,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        create_soulwell = {
+            id = 29893,
+            cast = 3,
+            cooldown = 120,
+            gcd = "spell",
+            
+            spend = 0.05,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136194,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        dark_pact = {
+            id = 108416,
+            cast = 0,
+            cooldown = 60,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 538538,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        demonbolt = {
+            id = 264178,
+            cast = function () return buff.demonic_core.up and 0 or ( 4.5 * haste ) end,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 2032588,
+            
+            handler = function ()
+                removeStack( 'demonic_core' )
+                gain( 2, 'soul_shards' )
+            end,
+        },
+        
+
+        demonic_circle = {
+            id = 48018,
+            cast = 0.5,
+            cooldown = 10,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 237559,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        demonic_circle_teleport = {
+            id = 48020,
+            cast = 0,
+            cooldown = 30,
+            gcd = "spell",
+            
+            spend = 0.03,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 237560,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        demonic_gateway = {
+            id = 111771,
+            cast = 2,
+            cooldown = 10,
+            gcd = "spell",
+            
+            spend = 0.03,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 607512,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        demonic_strength = {
+            id = 267171,
+            cast = 0,
+            cooldown = 60,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 236292,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        doom = {
+            id = 265412,
+            cast = 0,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.01,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136122,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        drain_life = {
+            id = 234153,
+            cast = 0,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136169,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        enslave_demon = {
+            id = 1098,
+            cast = 3,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136154,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        eye_of_kilrogg = {
+            id = 126,
+            cast = 2,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.03,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136155,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        fear = {
+            id = 5782,
+            cast = 1.7,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.05,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136183,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        grimoire_felguard = {
+            id = 111898,
+            cast = 0,
+            cooldown = 90,
+            gcd = "spell",
+            
+            spend = 1,
+            spendType = "soul_shards",
+            
+            startsCombat = true,
+            texture = 136216,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        hand_of_guldan = {
+            id = 105174,
+            cast = 1.5,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 3,
+            spendType = "soul_shards",
+            
+            startsCombat = true,
+            texture = 535592,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        health_funnel = {
+            id = 755,
+            cast = 0,
+            cooldown = 0,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 136168,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        implosion = {
+            id = 196277,
+            cast = 0,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 2065588,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        mortal_coil = {
+            id = 6789,
+            cast = 0,
+            cooldown = 45,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 607853,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        nether_portal = {
+            id = 267217,
+            cast = 2.5,
+            cooldown = 180,
+            gcd = "spell",
+            
+            spend = 3,
+            spendType = "soul_shards",
+            
+            startsCombat = true,
+            texture = 2065615,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        power_siphon = {
+            id = 264130,
+            cast = 0,
+            cooldown = 30,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 236290,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        ritual_of_summoning = {
+            id = 698,
+            cast = 0,
+            cooldown = 120,
+            gcd = "spell",
+            
+            spend = 0,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136223,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        shadow_bolt = {
+            id = 686,
+            cast = 2,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136197,
+            
+            handler = function ()
+                gain( 1, 'soul_shards' )
+            end,
+        },
+        
+
+        shadowfury = {
+            id = 30283,
+            cast = 1.5,
+            cooldown = 60,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 607865,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        soul_strike = {
+            id = 264057,
+            cast = 0,
+            cooldown = 10,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 1452864,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        soulstone = {
+            id = 20707,
+            cast = 3,
+            cooldown = 600,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 136210,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        summon_demonic_tyrant = {
+            id = 265187,
+            cast = 2,
+            cooldown = 90,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 2065628,
+            
+            handler = function ()
+            end,
+        },
+
+        
+        summon_felguard = {
+            id = 30146,
+            cast = function () return 2.5 * haste end,
+            cooldown = 0,
+            gcd = "spell",
+
+            spend = 1,
+            spendType = "soul_shards",
+
+            startsCombat = false,
+            texture = 136216,
+
+            usable = function () return not pet.exists end,
+            handler = function ()
+                summonPet( 'felguard', 3600 )
+            end,
+        },
+
+
+        summon_vilefiend = {
+            id = 264119,
+            cast = 2,
+            cooldown = 45,
+            gcd = "spell",
+            
+            spend = 1,
+            spendType = "soul_shards",
+            
+            startsCombat = true,
+            texture = 1616211,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        unending_breath = {
+            id = 5697,
+            cast = 0,
+            cooldown = 0,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136148,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        unending_resolve = {
+            id = 104773,
+            cast = 0,
+            cooldown = 180,
+            gcd = "spell",
+            
+            spend = 0.02,
+            spendType = "mana",
+            
+            startsCombat = true,
+            texture = 136150,
+            
+            handler = function ()
+            end,
+        },
+        
+
+        --[[ wartime_ability = {
+            id = 264739,
+            cast = 0,
+            cooldown = 0,
+            gcd = "spell",
+            
+            startsCombat = true,
+            texture = 1518639,
+            
+            handler = function ()
+            end,
+        }, ]]
+    } )
+
+    spec:RegisterOptions( {
+        enabled = true,
+
+        aoe = 3,
+    
+        nameplates = false,
+        nameplateRange = 8,
+        
+        damage = true,
+        damageExpiration = 6,
+    
+        package = "Demonology",
+    } )
+
+end
