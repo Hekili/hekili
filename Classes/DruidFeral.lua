7a74f11a1ad246233da13b0ea68eb85c8ad21509
Hekili <Hekili@users.noreply.github.com>
2018-07-18 01:16:14 -0400

    Try to stabilize Shadowmeld recommendation in Feral.

--- Classes/DruidFeral.lua
+++ Classes/DruidFeral.lua
@@ -1439,7 +1439,7 @@ if UnitClassBase( 'player' ) == 'DRUID' then
         gcd = "off",
 
         usable = function () return boss and race.night_elf end,
-        recheck = function () return buff.incarnation.remains - 0.1, buff.incarnation.remains end,
+        recheck = function () return energy[ "time_to_" .. action.rake.cost ], buff.incarnation.remains - 0.1, buff.incarnation.remains end,
         handler = function ()
             applyBuff( "shadowmeld" )
         end,
