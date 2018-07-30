3ca8399805e3d9f434ec186a91256ae7471c6e09
Hekili <Hekili@users.noreply.github.com>
2018-07-30 12:00:02 -0400

    Don't try to show displays for a disabled spec.

--- UI.lua
+++ UI.lua
@@ -1304,7 +1304,12 @@ do
             actsActive[ a ] = nil
         end
 
-        if profile.enabled then
+        local specEnabled = GetSpecialization()
+        specEnabled = specEnabled and GetSpecializationInfo( specEnabled )
+        specEnabled = specEnabled and profile.specs[ specEnabled ]
+        specEnabled = specEnabled and specEnabled.enabled or false
+
+        if profile.enabled and specEnabled then
             for i, display in pairs( profile.displays ) do
                 if display.enabled then
                     if self.Config then
