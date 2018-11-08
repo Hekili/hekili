58188cc70a4e2f69c4479cb083171db4bee8aa5a
Hekili <Hekili@users.noreply.github.com>
2018-11-07 23:52:12 -0500

    Fury:  Apply GCD when Bladestorming.

--- Classes/WarriorFury.lua
+++ Classes/WarriorFury.lua
@@ -131,6 +131,7 @@ if UnitClassBase( 'player' ) == 'WARRIOR' then
 
     spec:RegisterHook( "reset_precast", function ()
         rageSpent = 0
+        if buff.bladestorm.up then setCooldown( "global_cooldown", max( cooldown.global_cooldown.remains, buff.bladestorm.remains ) ) end
     end )
 
 
