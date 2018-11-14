f4284e33be76f5178dce7cf7cd1e43ad6e68abfe
Hekili <Hekili@users.noreply.github.com>
2018-11-13 20:02:10 -0500

    Arms:  Stop support for buff.executioners_precision since it doesn't seem to work anyway.  (Executioner's Precision is a debuff.)

--- Classes/WarriorArms.lua
+++ Classes/WarriorArms.lua
@@ -246,8 +246,6 @@ if UnitClassBase( 'player' ) == 'WARRIOR' then
     spec:RegisterHook( "reset_precast", function ()
         rageSpent = 0
         if buff.bladestorm.up then setCooldown( "global_cooldown", max( cooldown.global_cooldown.remains, buff.bladestorm.remains ) ) end
-
-        buff.executioners_precision = debuff.executioners_precision
     end )
 
 
