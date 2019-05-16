d3681cf65ba507ff92d9b537bd1cfa1914ffed22
Hekili <hekili.tcn@gmail.com>
2019-05-16 08:35:09 -0400

    Variable:  Fix regenerating resource check.

--- Scripts.lua
+++ Scripts.lua
@@ -1127,7 +1127,8 @@ function scripts:LoadScripts()
                         if lua then 
                             -- If resources are checked, it's time-sensitive.
                             for k in pairs( GetResourceInfo() ) do
-                                if lua:find( k ) and state[ k ] and state[ k ].regenModel and state[ k ].regen ~= 0 then script.TimeSensitive = true; break end
+                                local resource = rawget( state, k )
+                                if lua:find( k ) and resource and ( resource.regenModel or resource.regen ~= 0 ) then script.TimeSensitive = true; break end 
                                 -- if lua:find( k ) then script.TimeSensitive = true; break end
                             end
 
