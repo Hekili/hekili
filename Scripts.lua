c5c68ad81f13acbeccb16dfaf8e2ea64b96292ce
Hekili <hekili.tcn@gmail.com>
2020-04-01 10:10:39 -0400

    Tweak time_to_combustion timing helper.

--- Scripts.lua
+++ Scripts.lua
@@ -416,9 +416,9 @@ do
         { "^action%.([a-z0-9_]+)%.in_flight_remains<=?(.-)$",
                                                     "action.%1.in_flight_remains-%2" }, -- Fire Mage, but others too, potentially.
         { "^variable.time_to_combustion$",      "variable.time_to_combustion" },
-        { "^variable.time_to_combustion<=(.-)$",
+        { "^variable.time_to_combustion<=?(.-)$",
                                                 "variable.time_to_combustion-%1" },
-                                                
+
         { "^(pet%.[a-z0-9_]+)%.up",             "%1.remains" },
         { "^(pet%.[a-z0-9_]+)%.active",         "%1.remains" },
 
