3f987bd79653388f65a931c8f60af72cebc5f728
Hekili <hekili.tcn@gmail.com>
2020-10-25 21:45:48 -0400

    Bracket some recheck math since a-b+c is different from a-(b+c).

--- Scripts.lua
+++ Scripts.lua
@@ -514,7 +514,7 @@ do
             for key in pairs( decreases ) do
                 if lhs:match( key ) then
                     if comp == "<" then
-                        return true, lhs .. " + 0.01 - " .. rhs
+                        return true, "(" .. lhs .. " + 0.01) - (" .. rhs .. ")"
                     elseif lessOrEqual[ comp ] then
                         return true, lhs .. " - " .. rhs
                     end
@@ -524,7 +524,7 @@ do
             for key in pairs( increases ) do
                 if lhs:match( key ) then
                     if comp == ">" then
-                        return true, rhs .. " + 0.01 - " .. rhs
+                        return true, "(" .. rhs .. " + 0.01) - (" .. rhs .. ")"
                     elseif moreOrEqual[ comp ] then
                         return true, rhs .. " - " .. lhs
                     end
