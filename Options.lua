9af1414651d50ff55ca4fdf94cdd3b6485c00ff2
Hekili <hekili.tcn@gmail.com>
2020-11-25 09:16:42 -0500

    Tweak importer again.

--- Options.lua
+++ Options.lua
@@ -9097,7 +9097,7 @@ local function Sanitize( segment, i, line, warnings )
         table.insert( warnings, "Line " .. line .. ": Converted 'covenant.X.enabled' to 'covenant.X' (" .. times .. "x)." )
     end
 
-    i, times = i:gsub( "talent%.([%w_]+)([%+%-%*%%/&| ()<>])", "talent.%1.enabled%2" )
+    i, times = i:gsub( "talent%.([%w_]+)([%+%-%*%%/&|= ()<>])", "talent.%1.enabled%2" )
     if times > 0 then
         table.insert( warnings, "Line " .. line .. ": Converted 'talent.X' to 'talent.X.enabled' (" .. times .. "x)." )
     end
@@ -9107,7 +9107,7 @@ local function Sanitize( segment, i, line, warnings )
         table.insert( warnings, "Line " .. line .. ": Converted 'talent.X' to 'talent.X.enabled' at EOL (" .. times .. "x)." )
     end
 
-    i, times = i:gsub( "legendary%.([%w_]+)([%+%-%*%%/&| ()<>])", "legendary.%1.enabled%2" )
+    i, times = i:gsub( "legendary%.([%w_]+)([%+%-%*%%/&|= ()<>])", "legendary.%1.enabled%2" )
     if times > 0 then
         table.insert( warnings, "Line " .. line .. ": Converted 'legendary.X' to 'legendary.X.enabled' (" .. times .. "x)." )
     end
@@ -9117,17 +9117,17 @@ local function Sanitize( segment, i, line, warnings )
         table.insert( warnings, "Line " .. line .. ": Converted 'legendary.X' to 'legendary.X.enabled' at EOL (" .. times .. "x)." )
     end
 
-    i, times = i:gsub( "(^%.)runeforge%.([%w_]+)([%+%-%*%%/&| ()<>)])", "%1runeforge.%2.enabled%3" )
+    i, times = i:gsub( "([^%.])runeforge%.([%w_]+)([%+%-%*%%/=&| ()<>])", "%1runeforge.%2.enabled%3" )
     if times > 0 then
         table.insert( warnings, "Line " .. line .. ": Converted 'runeforge.X' to 'runeforge.X.enabled' (" .. times .. "x)." )
     end
 
-    i, times = i:gsub( "(^%.)runeforge%.([%w_]+)$", "%1runeforge.%2.enabled" )
+    i, times = i:gsub( "([^%.])runeforge%.([%w_]+)$", "%1runeforge.%2.enabled" )
     if times > 0 then
         table.insert( warnings, "Line " .. line .. ": Converted 'runeforge.X' to 'runeforge.X.enabled' at EOL (" .. times .. "x)." )
     end
 
-    i, times = i:gsub( "^runeforge%.([%w_]+)([%+%-%*%%/&| ()<>)])", "runeforge.%1.enabled%2" )
+    i, times = i:gsub( "^runeforge%.([%w_]+)([%+%-%*%%/&|= ()<>)])", "runeforge.%1.enabled%2" )
     if times > 0 then
         table.insert( warnings, "Line " .. line .. ": Converted 'runeforge.X' to 'runeforge.X.enabled' (" .. times .. "x)." )
     end
@@ -9137,7 +9137,7 @@ local function Sanitize( segment, i, line, warnings )
         table.insert( warnings, "Line " .. line .. ": Converted 'runeforge.X' to 'runeforge.X.enabled' at EOL (" .. times .. "x)." )
     end
 
-    i, times = i:gsub( "conduit%.([%w_]+)([%+%-%*%%/&| ()<>)])", "conduit.%1.enabled%2" )
+    i, times = i:gsub( "conduit%.([%w_]+)([%+%-%*%%/&|= ()<>)])", "conduit.%1.enabled%2" )
     if times > 0 then
         table.insert( warnings, "Line " .. line .. ": Converted 'conduit.X' to 'conduit.X.enabled' (" .. times .. "x)." )
     end
