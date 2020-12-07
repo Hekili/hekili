1960d48a5aa940d47037ae7a1f31e7cf41015172
Hekili <hekili.tcn@gmail.com>
2020-12-07 10:14:52 -0500

    Make time_to_hpg work nicely in alternate displays.

--- Classes/PaladinRetribution.lua
+++ Classes/PaladinRetribution.lua
@@ -481,7 +481,7 @@ if UnitClassBase( "player" ) == "PALADIN" then
     end )
 
     spec:RegisterStateExpr( "time_to_hpg", function ()
-        return max( gcd.remains, min( cooldown.judgment.remains, cooldown.crusader_strike.remains, cooldown.blade_of_justice.remains, ( action.hammer_of_wrath.usable and cooldown.hammer_of_wrath.remains or 999 ), cooldown.wake_of_ashes.remains, ( race.blood_elf and cooldown.arcane_torrent.remains or 999 ), ( IsSpellKnown( 304971 ) and cooldown.divine_toll.remains or 999 ) ) )
+        return max( gcd.remains, min( cooldown.judgment.true_remains, cooldown.crusader_strike.true_remains, cooldown.blade_of_justice.true_remains, ( action.hammer_of_wrath.usable and cooldown.hammer_of_wrath.true_remains or 999 ), cooldown.wake_of_ashes.true_remains, ( race.blood_elf and cooldown.arcane_torrent.true_remains or 999 ), ( IsSpellKnown( 304971 ) and cooldown.divine_toll.true_remains or 999 ) ) )
     end )
 
     spec:RegisterHook( "reset_precast", function ()
