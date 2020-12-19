52ef17ca2ca681735dc28ae558ef94b18652874c
Hekili <hekili.tcn@gmail.com>
2020-12-18 23:58:43 -0500

    Differentiate the anima power buff for NET from the proc that you get when it happens.  Fixes #714.

--- Classes/PaladinRetribution.lua
+++ Classes/PaladinRetribution.lua
@@ -960,7 +960,7 @@ if UnitClassBase( "player" ) == "PALADIN" then
             startsCombat = true,
             texture = 613533,
 
-            usable = function () return target.health_pct < 20 or ( level > 57 and ( buff.avenging_wrath.up or buff.crusade.up ) ) or buff.final_verdict.up or buff.hammer_of_wrath_hallow.up or buff.negative_energy_token.up end,
+            usable = function () return target.health_pct < 20 or ( level > 57 and ( buff.avenging_wrath.up or buff.crusade.up ) ) or buff.final_verdict.up or buff.hammer_of_wrath_hallow.up or buff.negative_energy_token_proc.up end,
             handler = function ()
                 removeBuff( "final_verdict" )
 
@@ -981,7 +981,8 @@ if UnitClassBase( "player" ) == "PALADIN" then
                     max_stack = 3
                 },
 
-                negative_energy_token = {
+                -- Power: 335069
+                negative_energy_token_proc = {
                     id = 345693,
                     duration = 5,
                     max_stack = 1,
