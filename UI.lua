43418888b56f4c49079f6b528a128794f5d1ed53
Hekili <hekili.tcn@gmail.com>
2022-10-26 11:32:32 -0400

    Death Knights.

--- UI.lua
+++ UI.lua
@@ -2166,7 +2166,7 @@ do
                 if coroutine.status( thread ) == "dead" or err then
 
                     if Hekili.ActiveDebug then
-                        Hekili:Debug( "Recommendation thread for %s terminated due to error: %s", self.id, err )
+                        Hekili:Debug( format( "Recommendation thread for %s terminated due to error: %s", self.id, err and err:gsub( "%%", "%%%%" ) or "Unknown" ) )
                         Hekili:SaveDebugSnapshot( self.id )
                         Hekili.ActiveDebug = nil
                     end
