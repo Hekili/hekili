d2eeb64c4286bb421fbb2e365cb20209960f4373
IIeTpoc <64799395+IIeTpoc@users.noreply.github.com>
2022-10-27 18:49:20 +0300

    Update RogueOutlaw.lua
    Flagellation should be used from Shadowlands Covenant lua file

--- Dragonflight/RogueOutlaw.lua
+++ Dragonflight/RogueOutlaw.lua
@@ -1201,66 +1201,67 @@ spec:RegisterAbilities( {
             applyBuff( "feint", 5 )
         end,
     },
-        flagellation = {
-            id = 323654,
-            cast = 0,
-            cooldown = 90,
-            gcd = "spell",
+        -- flagellation should be used from the Shadowlands covenant lua file
+        --flagellation = {
+            --id = 323654,
+            --cast = 0,
+            --cooldown = 90,
+            --gcd = "spell",
 
-            spend = 0,
-            spendType = "energy",
+            --spend = 0,
+            --spendType = "energy",
 
-            startsCombat = true,
-            texture = 3565724,
+            --startsCombat = true,
+            --texture = 3565724,
 
-            toggle = "essences",
+            --toggle = "essences",
 
-            indicator = function ()
-                if settings.cycle and args.cycle_targets == 1 and active_enemies > 1 and target.time_to_die < longest_ttd then
-                    return "cycle"
-                end
-            end,
+            --indicator = function ()
+                --if settings.cycle and args.cycle_targets == 1 and active_enemies > 1 and target.time_to_die < longest_ttd then
+                    --return "cycle"
+                --end
+            --end,
 
-            handler = function ()
-                applyBuff( "flagellation" )
-                applyDebuff( "target", "flagellation", 12 )
-            end,
+            --handler = function ()
+                --applyBuff( "flagellation" )
+                --applyDebuff( "target", "flagellation", 12 )
+            --end,
 
-            auras = {
-                flagellation = {
-                    id = 323654,
-                    duration = 12,
-                    max_stack = 30,
-                    generate = function( t, aType )
-                        local unit, func
+            --auras = {
+                --flagellation = {
+                    --id = 323654,
+                    --duration = 12,
+                    --max_stack = 30,
+                    --generate = function( t, aType )
+                        --local unit, func
 
-                        if aType == "debuff" then
-                            unit = "target"
-                            func = FindUnitDebuffByID
-                        else
-                            unit = "player"
-                            func = FindUnitBuffByID
-                        end
+                        --if aType == "debuff" then
+                            --unit = "target"
+                           -- func = FindUnitDebuffByID
+                       -- else
+                            --unit = "player"
+                            --func = FindUnitBuffByID
+                       -- end
 
-                        local name, _, count, _, duration, expires, caster = func( unit, 323654 )
+                        --local name, _, count, _, duration, expires, caster = func( unit, 323654 )
 
-                        if name then
-                            t.count = 1
-                            t.expires = expires
-                            t.applied = expires - duration
-                            t.caster = "player"
-                            return
-                        end
+                        --if name then
+                           -- t.count = 1
+                            --t.expires = expires
+                            --t.applied = expires - duration
+                            --t.caster = "player"
+                           -- return
+                       -- end
 
-                        t.count = 0
-                        t.expires = 0
-                        t.applied = 0
-                        t.caster = "nobody"
-                    end,
-                    copy = "flagellation_buff"
-                },
-            },
-        },
+                        --t.count = 0
+                       -- t.expires = 0
+                       -- t.applied = 0
+                        --t.caster = "nobody"
+                    --end,
+                    --copy = "flagellation_buff"
+               -- },
+            --},
+       -- },
             
     ghostly_strike = {
         id = 196937,
