411ade9ef9f4e51fcc75101e4ac26ab1ebd02651
Hekili <hekili.tcn@gmail.com>
2023-04-11 12:01:26 -0400

    Rogues: Forcibly sync Cold Blood to the GCD (#2520)

--- Dragonflight/RogueAssassination.lua
+++ Dragonflight/RogueAssassination.lua
@@ -1661,6 +1661,7 @@ spec:RegisterAbilities( {
         talent = "cold_blood",
         startsCombat = false,
         nobuff = "cold_blood",
+        readyTime = function() return gcd.remains end,
 
         handler = function ()
             applyBuff( "cold_blood" )
