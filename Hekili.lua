d1aabd065a70c852dc0aeb8c0fe3d47ccd904bdf
Hekili <hekili.tcn@gmail.com>
2023-07-11 23:31:57 -0400

    Augmentation is ready.

--- Hekili.lua
+++ Hekili.lua
@@ -41,7 +41,7 @@ ns.PTR = buildNum > 100105
 
 
 ns.Patrons = "|cFFFFD100Current Status|r\n\n"
-    .. "All existing specializations are currently supported, though healer priorities are experimental and focused on rotational DPS only.  Augmentation Evoker to follow very soon.\n\n"
+    .. "All existing specializations are currently supported, though healer priorities are experimental and focused on rotational DPS only.\n\n"
     .. "If you find odd recommendations or other issues, please follow the |cFFFFD100Issue Reporting|r link below and submit all the necessary information to have your issue investigated.\n\n"
     .. "Please do not submit tickets for routine priority updates (i.e., from SimulationCraft).  I will routinely update those when they are published.  Thanks!"
 
