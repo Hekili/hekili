6ee9d0f7ca2bc6873ce03b7be94afa2aba28e475
Hekili <trobbins@gmail.com>
2023-11-15 18:14:30 -0500

    Revert Summon Gargoyle "fix."

--- Dragonflight/DeathKnightUnholy.lua
+++ Dragonflight/DeathKnightUnholy.lua
@@ -1959,8 +1959,7 @@ me:RegisterAbilities( {
 
     -- Talent: Summon a Gargoyle into the area to bombard the target for $61777d.    The Gar...
     summon_gargoyle = {
-        id = function() return IsUsableSpell( 207349 ) and 207349 or 49206 end,
-        known = 49206,
+        id = 49206,
         cast = 0,
         cooldown = 180,
         gcd = "off",
@@ -1975,7 +1974,7 @@ me:RegisterAbilities( {
             gain( 50, "runic_power" )
         end,
 
-        copy = { "dark_arbiter", 207349 }
+        copy = { 207349 }
     },
 
     -- Talent: Strike your target dealing $s2 Shadow damage, infecting the target with $s3 F...
