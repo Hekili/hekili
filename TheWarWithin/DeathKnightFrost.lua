eddd3a0004c2ba65e7c600be9926be2874524851
Hekili <Hekili@users.noreply.github.com>
2024-08-08 19:15:31 -1000

    Frost DK: Razorice target swap (maybe)

--- TheWarWithin/DeathKnightFrost.lua
+++ TheWarWithin/DeathKnightFrost.lua
@@ -1414,7 +1414,7 @@ spec:RegisterAbilities( {
 
         cycle = function ()
             if debuff.mark_of_fyralath.up then return "mark_of_fyralath" end
-            if death_knight.runeforge.razorice then return "razorice" end
+            if death_knight.runeforge.razorice and debuff.razorice.stack == 5 then return "razorice" end
         end,
 
         handler = function ()
@@ -1614,7 +1614,7 @@ spec:RegisterAbilities( {
 
         cycle = function ()
             if debuff.mark_of_fyralath.up then return "mark_of_fyralath" end
-            if death_knight.runeforge.razorice then return "razorice" end
+            if death_knight.runeforge.razorice and debuff.razorice.stack == 5 then return "razorice" end
         end,
 
         handler = function ()
