2555fc1e7ddb8270d0105b58304ecd5b149ae37c
syrifgit <139663837+syrifgit@users.noreply.github.com>
2025-01-18 01:30:03 -0400

    Vengeance rework + 11.1 stuff

--- TheWarWithin/DemonHunterHavoc.lua
+++ TheWarWithin/DemonHunterHavoc.lua
@@ -1,5 +1,5 @@
 -- DemonHunterHavoc.lua
--- July 2024
+-- January 2025
 
 if UnitClassBase( "player" ) ~= "DEMONHUNTER" then return end
 
@@ -2120,7 +2120,7 @@ spec:RegisterAbilities( {
     spectral_sight = {
         id = 188501,
         cast = 0,
-        cooldown = 30,
+        cooldown = function() return 30 - ( 5 * talent.lost_in_darkness.rank ) end,
         gcd = "spell",
         school = "physical",
 
