fd0567f66b036357c575bec8a2c66b492d1cd348
Hekili <Hekili@users.noreply.github.com>
2025-08-20 16:34:42 -1000

    Fix frostreaper aura key

--- TheWarWithin/DeathKnightFrost.lua
+++ TheWarWithin/DeathKnightFrost.lua
@@ -1551,7 +1551,7 @@ spec:RegisterAbilities( {
             if talent.icy_onslaught.enabled then addStack( "icy_onslaught" ) end
             removeBuff( "frostbane" )
             if death_knight.runeforge.razorice then applyDebuff( "target", "razorice", nil, min( 5, debuff.razorice.stack + 1 ) ) end
-            if talent.frostreaper.enabled then removeBuff( "frost_reaper" ) end
+            if talent.frostreaper.enabled then removeBuff( "frostreaper" ) end
             -- Legacy / PvP
             if pvptalent.bitter_chill.enabled and debuff.chains_of_ice.up then
                 applyDebuff( "target", "chains_of_ice" )
