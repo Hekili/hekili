72e414c87b06227d7cf4c0c341b8adc9f8cd8de6
syrifgit <139663837+syrifgit@users.noreply.github.com>
2025-08-26 00:26:52 -0300

    texture

--- TheWarWithin/RogueAssassination.lua
+++ TheWarWithin/RogueAssassination.lua
@@ -2756,7 +2756,7 @@ spec:RegisterAbilities( {
         recharge = 60,
         icd = 1,
         gcd = "off",
-        school = "physical",
+        texture = 381623,
 
         spend = -100,
         spendType = "energy",
