308e9a0e0953be4408c74e5f81dfa7a56037f080
Hekili <Hekili@users.noreply.github.com>
2025-08-30 14:42:07 -1000

    Fix Voidbinding CDR debug message

--- State.lua
+++ State.lua
@@ -3339,7 +3339,7 @@ do
                     if state.debuff.voidbinding.up and modRate and modRate ~= 1 then
                         local extraTime = start + duration - state.query_time - state.debuff.voidbinding.remains
                         if extraTime > 0 then
-                            if Hekili.ActiveDebug then Hekili:Debug( "Extending '%s' remaining cooldown by %.2f because the cooldown exceeds Voidbinding's remaining time by %.2f.", ( extraTime * 0.3 ), extraTime ) end
+                            if Hekili.ActiveDebug then Hekili:Debug( "Extending '%s' remaining cooldown by %.2f because the cooldown exceeds Voidbinding's remaining time by %.2f.", ability.key, ( extraTime * 0.3 ), extraTime ) end
                             duration = duration + ( extraTime * 0.3 )
                         end
                     end
